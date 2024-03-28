4fe1c1bccdaea9ca6ef77f8f284762c1c13cc993
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2024-03-29 00:56:53 +0200

    exec 2023-03-2+1

--- src/main/java/ua/in/sz/git/module/a/ModuleA.java
+++ src/main/java/ua/in/sz/git/module/a/ModuleA.java
@@ -2,6 +2,6 @@ package ua.in.sz.git.module.a;
 
 public class ModuleA {
     public static void exec() {
-        System.out.println("exec 2023-03-29: " + ModuleA.class.getName());
+        System.out.println("exec 2023-03-29 +1: " + ModuleA.class.getName());
     }
 }
